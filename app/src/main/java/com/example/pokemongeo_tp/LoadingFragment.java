26e48be6bc8c96f47b2131f7d0551f533a85edfa
jo-pouradier <jocharbo43@gmail.com>
2024-10-28 14:44:51 +0100

    better loading

--- app/src/main/java/com/example/pokemongeo_tp/LoadingFragment.java
+++ app/src/main/java/com/example/pokemongeo_tp/LoadingFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 
 public class LoadingFragment extends Fragment {
 
@@ -20,6 +23,16 @@ public class LoadingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_loading, container, false);
+        // create spinner loading
+        ProgressBar spinner = new ProgressBar(getContext());
+        spinner.setIndeterminate(true);
+        spinner.setVisibility(View.VISIBLE);
+        FrameLayout.LayoutParams frameLayoutParams = new FrameLayout.LayoutParams(500,500, Gravity.CENTER);
+        spinner.setLayoutParams(frameLayoutParams);
+
+        View view = inflater.inflate(R.layout.fragment_loading, container, false);
+        // add spinner to view
+        ((ViewGroup) view).addView(spinner);
+        return view;
     }
 }
